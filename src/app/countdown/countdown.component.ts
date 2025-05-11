c1944b5374876f949f88e32b72a0ed7777609a86
Meezan Shaikh <meezan@tradexa.co.in>
2025-05-11 22:24:05 +0530

    change detection strategy updated to onpush on countdown component

--- src/app/countdown/countdown.component.ts
+++ src/app/countdown/countdown.component.ts
@@ -1,11 +1,12 @@
-import { Component, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-countdown',
   standalone: true,
   imports: [],
   templateUrl: './countdown.component.html',
-  styleUrl: './countdown.component.scss'
+  styleUrl: './countdown.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CountdownComponent {
   secondsLeft = input<number | null>(0)
