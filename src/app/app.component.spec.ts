7ee9f84f0e053440ad989285f0d4680e0c9c86f5
Meezan Shaikh <meezan@tradexa.co.in>
2025-05-11 21:04:17 +0530

    app setup and countdown logic with api implemented

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,12 +14,6 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'six-crickets' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('six-crickets');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { AsyncPipe, JsonPipe } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { Observable, catchError, interval, map, of, switchMap, take } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [AsyncPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'six-crickets';
+
+  http = inject(HttpClient);
+
+  secondsLeft$: Observable<any> = this.http.get("https://run.mocky.io/v3/5b4f3255-5691-46bc-a249-bea56e4a62cd").pipe(
+    switchMap((response: any) =>
+      interval(1000).pipe(
+        take(response.secondLeft + 1), // emit one value every second, total: seconds + 1 times (including 0)
+        map(i => response.secondLeft - i)
+      )
+    ),
+    catchError(()=>{
+      return of(null)
+    })
+  )
 }
